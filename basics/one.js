761efe928a2382e066d782e19f3d6e18b0982487
aswinmargy16 <aswinmargy16@gmail.com>
2023-02-26 10:04:38 +0530

    node

---
+++ basics/one.js
@@ -0,0 +1,4 @@
+console.log("Req A")
+console.log("Req B")
+console.log("Req C")
+console.log("Req D")
---
+++ basics/two.js
@@ -0,0 +1,12 @@
+setTimeout(() => {
+    console.log("Req A")
+}, 5000)
+setTimeout(() => {
+    console.log("Req B")
+}, 20000)
+setTimeout(() => {
+    console.log("Req C")
+}, 2000)
+setTimeout(() => {
+    console.log("Req D")
+}, 3000)
